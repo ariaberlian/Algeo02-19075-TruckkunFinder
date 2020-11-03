2d78d85bff283cb154e3892fc4cd249aecf5e20c
ariaberlian <59786236+ariaberlian@users.noreply.github.com>
2020-11-04 01:09:13 +0700

    Delete webscrap.py

--- searchEngine/webscrap.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-def webscrap(lokasi):
-    f = open(lokasi, "r")
-    html = f.read()
-    soup = BeautifulSoup(html, features="html.parser")
-    text = str(soup.get_text)
-    f.close()
-    return text
